c1b3c49b5a2aee4479660cecf0ad2941f47b4d01
Aditya Paul <122125787+Paulie-Aditya@users.noreply.github.com>
2023-08-16 17:44:39 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find Transition Point/find-transition-point.py
@@ -0,0 +1,22 @@
+def transitionPoint(arr, n):
+    #Code here
+    try:
+        return arr.index(1)
+    except:
+        return -1
+
+
+
+
+
+#{ 
+ # Driver Code Starts
+#driver code
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        print(transitionPoint(arr, n))
+
+# } Driver Code Ends
