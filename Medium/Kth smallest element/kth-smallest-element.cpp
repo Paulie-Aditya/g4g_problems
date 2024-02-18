feeba8446063842f3f9465c937a915caee940817
Aditya Paul <122125787+Paulie-Aditya@users.noreply.github.com>
2024-02-18 16:05:28 +0530

    Updated Solution - GfG to GitHub

--- Medium/Kth smallest element/kth-smallest-element.cpp
+++ Medium/Kth smallest element/kth-smallest-element.cpp
@@ -7,21 +7,28 @@ using namespace std;
 // } Driver Code Ends
 //User function template for C++
 
-
 class Solution{
     public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
     int kthSmallest(int arr[], int l, int r, int k) {
-        priority_queue<int> max_heap;
-        int size = 0;
-        for(int i = l; i<=r; i++) {
-            max_heap.push(arr[i]);
-            size++;
-            if (size > k) {
-                max_heap.pop();
-                size--;
+        priority_queue<int> pq;
+        
+        for(int i = 0; i<k ; i++){
+            pq.push(arr[i]);
+        }
+        
+        for(int i = k; i<= r; i++){
+            if(arr[i] < pq.top()){
+                pq.pop();
+                pq.push(arr[i]);
             }
         }
-        return max_heap.top();
+        
+        
+        return pq.top();
     }
 };
 
